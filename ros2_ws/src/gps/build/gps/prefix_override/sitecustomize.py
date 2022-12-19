1ba06eafd40e9acbaae3bd70cc2fb7aa8b0f2ac1
Wojciech Siudy <wojcsiu541@student.polsl.pl>
2022-12-19 21:17:55 +0100

    GPS support

---
+++ ros2_ws/src/gps/build/gps/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/wojtek/pbl/pbl-ford-ka/ros2_ws/src/gps/install/gps'
---
+++ ros2_ws/src/gps/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'gps'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wojtek',
+    maintainer_email='wojcsiu541@student.polsl.pl',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = gps.position_publisher:main',
+            'listener = gps.position_subscriber:main',
+        ],
+    },
+)
