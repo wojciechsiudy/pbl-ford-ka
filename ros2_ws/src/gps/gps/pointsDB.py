60a0a2e7c5315f129c706fc32ca14dff45d6c540
Wojciech Siudy <wojcsiu541@student.polsl.pl>
2023-01-16 11:33:31 +0100

    Introduce json database to gps package

--- ros2_ws/src/gps/gps/pointsDB.py
+++ ros2_ws/src/gps/gps/pointsDB.py
@@ -1,17 +1,26 @@
 from gps.ka_utils import Point
+import json
+
+def read_json_file():
+    f = open("/home/wojtek/pbl/pbl-ford-ka/ros2_ws/src/gps/gps/GPSdata.json")
+    return json.load(f)
+
+def load_points_from_json():
+    points = []
+    for line in read_json_file():
+        points.append(Point(line['x'], line['y'], line ['address']))
+    return points
 
 def getPoints(subset):
-    if subset == 1:
+    if subset == 0:
         return [
             Point(50.290138, 18.677277, "AA:BB"),
             Point(50.289368, 18.678203, "CC:DD")]
+    elif subset == 1:
+        return load_points_from_json()
     else:
         return None
 
-position = getPoints(1)[0]
-msg = '%f;%f' % (position.x, position.y)
-print(msg)
-
-raw_coordinates = msg.split(';')
-coordinates = Point(float(raw_coordinates[0]), float(raw_coordinates[1]))
-print('Listening: %f;%f' % (coordinates.x, coordinates.y))
+#test
+for point in getPoints(1):
+    print(point.x, point.y, point.address)
